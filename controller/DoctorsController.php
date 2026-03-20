66212eefe2f9ddd2cf2995b10529ae72c7555c0d
IO-OI053 <hnduya25053@cusc.ctu.edu.vn>
2026-03-20 10:50:04 +0700

    Refactor DoctorsController for better request handling

--- controller/DoctorsController.php
+++ controller/DoctorsController.php
@@ -1,44 +1,44 @@
 <?php
-require_once . './models/Doctors.php';
+require_once('../models/doctors.php');
 
-class DoctorsController {
+function handleDoctorsRequest($conn) {
 
-    private $model;
+    if (isset($_POST['btnAdd'])) {
+        $username = $_POST['username'];
+        $email    = $_POST['email'];
+        $password = $_POST['password'];
+        $fullname = $_POST['fullname'];
+        $phone    = $_POST['phone'];
 
-    public function __construct() {
-        $this->model = new Doctors();
+        createDoctor($conn, $username, $email, $password, $fullname, $phone);
+        header("Location: manage_doctors.php");
+        exit();
     }
 
-    public function handleRequest() {
+    if (isset($_POST['btnEdit'])) {
+        $doctor_id = (int)$_POST['doctor_id'];
+        $fullname  = $_POST['fullname'];
+        $email     = $_POST['email'];
+        $phone     = $_POST['phone'];
 
-
-        if (isset($_POST['add'])) {
-
-            $username = $_POST['username'];
-            $email    = $_POST['email'];
-            $password = $_POST['password'];
-            $fullname = $_POST['fullname'];
-            $phone    = $_POST['phone'];
-
-            $this->model->create($username, $email, $password, $fullname, $phone);
-
-            header("Location: ./views/admin/manage_doctors.php");
-            exit();
-        }
-
-
-        if (isset($_GET['delete'])) {
-            $id = $_GET['delete'];
-
-            $this->model->delete($id);
-
-            header("Location: ./views/admin/manage_doctors.php");
-            exit();
-        }
+        updateDoctor($conn, $doctor_id, $fullname, $email, $phone);
+        header("Location: manage_doctors.php");
+        exit();
     }
 
-    public function getAll() {
-        return $this->model->getAll();
+
+    if (isset($_POST['btnDelete'])) {
+        $doctor_id = (int)$_POST['doctor_id'];
+        deleteDoctor($conn, $doctor_id);
+        header("Location: manage_doctors.php");
+        exit();
     }
+
+    if (isset($_GET['action']) && $_GET['action'] === 'edit') {
+        $doctor_id = (int)$_GET['id'];
+        return getDoctorById($conn, $doctor_id);
+    }
+
+    return null;
 }
 ?>
